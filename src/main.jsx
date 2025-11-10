3869028a5a40432f40194b6e35a105ee3443df99
hafeez1389 <hafeez.islam00@gmail.com>
2025-11-10 11:03:12 +0600

    Components setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './layouts/RootLayout.jsx';
+import Home from './components/Home/Home.jsx';
+import AddTransaction from './components/AddTransaction/AddTransaction.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'addTransaction',
+        Component: AddTransaction
+      }
+    ]
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />,
   </StrictMode>,
 )
